8acfaf3b7e4b3a858cd34dc713d5380df20718a8
Jorge <ezpadaz@me.com>
2021-04-18 21:25:14 -0500

    Image field

--- models/Ubicacion.js
+++ models/Ubicacion.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
  * Desc: Descripcion de la ubicacion.
  * tipo: (ex. zona_arqueologica)
  * ubicacion: (su latitud y longitud, se tiene que buscar con google maps.)
+ * image: una unica imagen de desripcion del lugar.
  */
 
 const UbicacionSchema = mongoose.Schema({
@@ -17,11 +18,16 @@ const UbicacionSchema = mongoose.Schema({
     },
     tipo: {
         type: String,
+        required: true
     },
     ubicacion: [{
         lat : String,
         lng : String
-    }]
+    }],
+    image: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports('Ubicaciones', UbicacionSchema);
