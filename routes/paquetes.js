a1aadd64385c824079fe88c9f2b21be7f0243b88
Jorge <ezpadaz@me.com>
2021-04-23 18:35:31 -0500

    Package get all packages done, also ID search done, needs frontend testing

--- routes/paquetes.js
+++ routes/paquetes.js
@@ -4,9 +4,26 @@ const Paquete = require('../models/Paquete');
 const auth = require('../middleware/verifyToken');
 
 router.get('/', auth, async(req,res) => {
-    res.json({
-        message: "Ruta de paquete."
-    });
+    try{
+        console.log('Received get-all packages.');
+        const allPackages = await Paquete.find();
+        res.status(200);
+        res.json(allPackages);
+    }catch(err){
+        res.status(401);
+        res.json(err);
+    }
+});
+
+router.get('/:paqueteId', auth, async(req,res) => {
+    try{
+        const foundPackage = await Paquete.findById(req.params.paqueteId);
+        res.status(200);
+        res.json(foundPackage);
+    }catch(err){
+        res.status(401);
+        res.json(err);
+    }
 });
 
 router.post('/', auth, async (req, res) =>{
